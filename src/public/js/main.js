bcee02887453e8c74ce9cbc75d6907c62e9bcfe6
Mauricio <msevillab@gmail.com>
2019-10-30 15:10:09 -0500

    envio mensajes a consola navegador con messageBox.val

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,22 @@
-io();
+
+
+$(function(){
+
+    const socket = io();
+
+    // obtaining DOM
+
+    const $messageForm = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#chat');
+
+    // events
+
+    $messageForm.submit( e => {
+
+        e.preventDefault();
+        console.log($messageBox.val());
+    });
+
+
+})
