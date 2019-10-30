8e5811f8df549bcaaf1dfe5a9f8403e3c5f9fc41
Mauricio <msevillab@gmail.com>
2019-10-30 18:01:12 -0500

    ingreso de usuarios y normalización de espacios de los div con validación de usuarios existentes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,11 +4,44 @@ $(function(){
 
     const socket = io();
 
-    // obtaining DOM
+    // obtaining DOM interface
 
     const $messageForm = $('#message-form');
     const $messageBox = $('#message');
     const $chat = $('#chat');
+
+
+    // obtaining DOM nicknameForm
+
+    const $nickForm = $('#nickForm');
+    const $nickError = $('#nickError');
+    const $nickname = $('#nickname');
+
+    const $users = $('#usernames');
+
+
+    $nickForm.submit(e => {
+
+        e.preventDefault();
+        socket.emit('new user', $nickname.val(), data =>{
+            if (data) {
+
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            } else {
+                $nickError.html(`
+                
+                <div class="alert alert-danger"> El usuario actualmente existe </div>`
+                
+                );
+                
+            }
+
+            $nickname.val('');
+
+
+        });
+    })
 
     // events
 
--- src/sockets.js
+++ src/sockets.js
@@ -2,15 +2,35 @@
 
 module.exports = function (io) {
 
+
+    let nicknames = [];
+
     io.on('connection', socket => {
 
         console.log("nuevo usuario conectado");
+
+        socket.on('new user', (data, cb) => {
+             
+
+            if (nicknames.indexOf(data) != -1) {
+
+                cb(false);
+            } else {
+                cb(true);
+                socket.nickname = data;
+                nicknames.push(socket.nickname);
+
+            }
+
+        })
 
 
         socket.on('send message', function(data){
 
           io.sockets.emit('new message', data);          
-        });    
+        });
+        
+        
     });
 
 
